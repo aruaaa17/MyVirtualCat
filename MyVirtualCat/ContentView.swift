312f8bbb9eaadb25058525074522107f8d2e44b6
April Zhang <aprilzh23@gmail.com>
2023-07-25 22:12:20 -0700

    set up basic ARView @ContentView

--- MyVirtualCat/ContentView.swift
+++ MyVirtualCat/ContentView.swift
@@ -16,22 +16,15 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    // Set up basic ARView
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    func updateUIView(_ uiView: ARView, context: Context) {
+    }
 }
 
 #if DEBUG
