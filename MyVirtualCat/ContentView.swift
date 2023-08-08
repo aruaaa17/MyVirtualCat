ca8e90b69a0c8d0c53091457ad076d415faaf5bd
April Zhang <aprilzh23@gmail.com>
2023-08-08 15:18:06 -0700

    create helper function for modelPicker

--- MyVirtualCat/ContentView.swift
+++ MyVirtualCat/ContentView.swift
@@ -176,43 +176,7 @@ class CustomARView: ARView{
 //    }
 //}
 
-struct ModelPickerView: View {
-    
-    var models: [Model]
-//    let vm: IconViewModel
-    @Binding var isPlacementEnable: Bool
-    @Binding var selectedModel: Model?
-    
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 25) {
-//                Spacer()
-                ForEach(0 ..< self.models.count, id: \.self) { index in
-                    Button(action: {
-                        print("DEBUG : selected model with name : \(self.models[index].modelName)")
-                        self.selectedModel = self.models[index]
-                        self.isPlacementEnable = true
-                        
-                    }, label: {
-                        Image(uiImage: self.models[index].image)
-                            .resizable()
-                            .cornerRadius(15)
-                            .frame(width: 80, height: 80)
-                            .aspectRatio(1/1,contentMode: .fill)
-                            .background(Color.white)
-//                            .border(.black, width: vm.selectedIcon == self.models[index] ? 1.0: 0.0)
-//                            .background(Color.white)
-//                            .onTapGesture { vm.selectedIcon = self.models[index] }
-                    })
-                    .buttonStyle(PlainButtonStyle())
-                    Spacer()
-                }
-            }
-        }
-        .padding(20)
-        .background(Color.black.opacity(0.5))
-    }
-}
+
 
 struct PlacementButtonView: View {
     @Binding var isPlacementEnable: Bool
