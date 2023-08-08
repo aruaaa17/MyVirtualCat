f5e9aea678ef9cd441541671318b7c47ca6246ef
April Zhang <aprilzh23@gmail.com>
2023-08-08 15:22:21 -0700

    create helper function for CustomARView part; remove CustomARView from ContentView

--- MyVirtualCat/ContentView.swift
+++ MyVirtualCat/ContentView.swift
@@ -141,30 +141,7 @@ struct ARViewContainer: UIViewRepresentable {
 //    }
 }
 
-class CustomARView: ARView{
-    var focusEntity: FocusEntity?
-    required init(frame frameRect: CGRect) {
-        super.init(frame: frameRect)
-//        self.setupConfig()
-        focusEntity = FocusEntity(on: self, focus: .plane)
-//        focusSquare.viewDelegate = self
-//        focusSquare.delegate = self
-//        focusSquare.setAutoUpdate(to: true)
-    }
-    
-    @objc required dynamic init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    func setupARView(){
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal,.vertical]
-        config.environmentTexturing = .automatic
-        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh){
-            config.sceneReconstruction = .mesh
-        }
-        self.session.run(config)
-    }
-}
+
 
 //extension CustomARView: FEDelegate{
 //    func toTrackingState() {
